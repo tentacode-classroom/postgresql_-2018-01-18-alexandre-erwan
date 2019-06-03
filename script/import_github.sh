03a09d70738e3222aee16e95c0a55af1ec80dad7
AexandreGelin <alexandre.gelin@ynov.com>
2019-06-03 16:12:08 +0200

    1 et 2

---
+++ script/import_github.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://data.gharchive.org/2015-01-01-15.json.gz
+
+gunzip 2015-01-01-15.json
---
+++ script/setup_postgres.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main
+" | sudo tee --append /etc/apt/sources.list.d/pgdg.list
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+     sudo apt-get update
+
+
+sudo apt-get install postgresql-11
+
+
+psql -V
