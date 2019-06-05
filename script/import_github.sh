e03cb5a725dc086f28b2c94b7c48d83ea143069e
AexandreGelin <alexandre.gelin@ynov.com>
2019-06-05 10:40:19 +0200

    ajout du git ignore

--- script/import_github.sh
+++ script/import_github.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #suppression des fichier si ils existent deja
-sudo rm -rf ../ressources/2015-01-01-15-.json
+sudo rm -rf ../resources/2015-01-01-15-.json
 
 #recuperation du fichier sous forme d'archive
 wget https://data.gharchive.org/2015-01-01-15.json.gz
 
 #on extrait l'archive dans le fichier
 gunzip 2015-01-01-15.json
-sudo mv 2015-01-01-15.json ../ressources/2015-01-01-15.json
+sudo mv 2015-01-01-15.json ../resources/2015-01-01-15.json
 sudo rm -rf 2015-01-01-15.json.gz
