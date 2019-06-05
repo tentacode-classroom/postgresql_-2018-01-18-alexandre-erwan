b06b6bd751b98ecc5795bbc1da682482d33976dc
AexandreGelin <alexandre.gelin@ynov.com>
2019-06-05 10:36:37 +0200

    resources file

--- script/import_github.sh
+++ script/import_github.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #suppression des fichier si ils existent deja
-sudo rm -rf 2015-01-01-15.json.gz
-sudo rm -rf 2015-01-01-15-.json
+sudo rm -rf ../ressources/2015-01-01-15-.json
 
 #recuperation du fichier sous forme d'archive
 wget https://data.gharchive.org/2015-01-01-15.json.gz
 
 #on extrait l'archive dans le fichier
 gunzip 2015-01-01-15.json
+sudo mv 2015-01-01-15.json ../ressources/2015-01-01-15.json
+sudo rm -rf 2015-01-01-15.json.gz
